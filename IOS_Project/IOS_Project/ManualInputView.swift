d88742edf56c05cecdebeaabe93303c4206cce59
105590025 <105590025@ntut.org.tw>
2020-06-22 21:19:08 +0800

    更換view寫法

--- IOS_Project/IOS_Project/ManualInputView.swift
+++ IOS_Project/IOS_Project/ManualInputView.swift
@@ -9,7 +9,7 @@
 import SwiftUI
 
 struct ManualInputView: View {
-    let receiptPattern = "[A-Z]{2}[0-9]{8}"
+    let receiptPattern = "^[A-Z]{2}[0-9]{8}$"
     @State private var receiptDate = Date()
     @State private var receiptID = ""
     @State private var wrongInput = false
@@ -30,24 +30,25 @@ struct ManualInputView: View {
             }
             .padding([.leading, .trailing], 30)
             VStack(alignment: .center){
-                Button(action: {
-                                let matcher = MyRegex(self.receiptPattern)
-                                if matcher.match(input: self.receiptID) {
-                                    self.wrongInput = false
-                                }else {
-                                    self.wrongInput = true
-                                }
-                            }){
-                                Text("確認")
-                                    .padding(EdgeInsets(top: 5, leading: 25, bottom: 5, trailing: 25))
-                                    .font(.system(size: 18))
-                                    .background(Color.blue)
-                                    .foregroundColor(.white)
-                                    .cornerRadius(40)
-                            }
-                            .alert(isPresented: $wrongInput){
-                                Alert(title: Text("發票規格錯誤"))
-                            }
+                Button(action:
+                    {
+                        let matcher = MyRegex(self.receiptPattern)
+                        if matcher.match(input: self.receiptID) {
+                            self.wrongInput = false
+                        }else {
+                            self.wrongInput = true
+                        }
+                    }){
+                        Text("確認")
+                            .padding(EdgeInsets(top: 5, leading: 25, bottom: 5, trailing: 25))
+                            .font(.system(size: 18))
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(40)
+                    }
+                    .alert(isPresented: $wrongInput){
+                        Alert(title: Text("發票規格錯誤"))
+                    }
             }
             .padding(.top, 10)
             Spacer()
@@ -57,9 +58,6 @@ struct ManualInputView: View {
 
 struct ManualInputView_Previews: PreviewProvider {
     static var previews: some View {
-//        NavigationView{
-//            ManualInputView()
-//        }
         ManualInputView()
     }
 }
--- IOS_Project/IOS_Project/ReceiptLotteryView.swift
+++ IOS_Project/IOS_Project/ReceiptLotteryView.swift
@@ -7,38 +7,117 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ReceiptLotteryView: View {
     @State private var codeDetail = ""
     @State private var isSharePresented: Bool = false
     @State private var analyzer = Analyzer()
+    @State private var isLight = false
+    @State private var isNavigationBarHidden = true
+    @State private var isLottery = false
+    @State private var show = false
+    
     var body: some View {
-        VStack{
+        NavigationView{
             VStack{
-                ZStack(alignment: .leading){
-                    CodeScannerView(codeTypes: [.qr], completion: self.handleScan)
-                    OtherView()
+                VStack{
+                    ZStack(alignment: .leading){
+                        CodeScannerView(codeTypes: [.qr], completion: self.handleScan)
+                        HStack{
+                            Image("qrcode")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 150,height:150)
+                            .padding(.leading, 80)
+                            Spacer()
+                            VStack(spacing: 30){
+                                Button(action: {
+                                    if self.isLight == false{
+                                        self.isLight = true
+                                    }else{
+                                        self.isLight = false
+                                    }
+                                    self.toggleTorch(on: self.isLight)
+                                }) {
+                                    Image("idea")
+                                    .renderingMode(.original)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 30,height:30)
+                                }
+                                
+                                NavigationLink(destination: ManualInputView()){
+                                    Image("document")
+                                    .renderingMode(.original)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 30,height:30)
+                                }
+                                
+                                Button(action: {
+                                    self.isLottery = true
+                                }) {
+                                    Image("cup")
+                                    .renderingMode(.original)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 30,height:30)
+                                }.sheet(isPresented: $isLottery,content: {
+                                    LotteryView()
+                                })
+                                
+                            }
+                        .padding(30)
+                        }
+                    }
                 }
+                .frame(height: 300)
+                Text("內文: \(codeDetail)")
+                Text("辨識結果:\(analyzer.receiptDetail)")
+                Text("發票號: \(analyzer.tempReceipt.ID)")
+                Text("發票日期: \(analyzer.tempReceipt.Date)")
+                Spacer()
             }
-            .frame(height: 300)
-            Text("內文: \(codeDetail)")
-            Text("辨識結果:\(analyzer.receiptDetail)")
-            Text("發票號: \(analyzer.tempReceipt.ID)")
-            Text("發票日期: \(analyzer.tempReceipt.Date)")
-            Spacer()
+            .navigationBarTitle("掃描")
+            .navigationBarHidden(self.isNavigationBarHidden)
+            .onAppear{self.isNavigationBarHidden = true}
+            .onDisappear{self.isNavigationBarHidden = false}
         }
     }
     
     func handleScan(result: Result<String, CodeScannerView.ScanError>) {
         isSharePresented = false
-            switch result {
-            case .success(let code):
-                codeDetail = code
-                analyzer.transform(data: codeDetail)
-            case .failure(let error):
-                print("Scanning failed \(error)")
-            }
+        switch result {
+        case .success(let code):
+            codeDetail = code
+            analyzer.transform(data: codeDetail)
+        case .failure(let error):
+            print("Scanning failed \(error)")
         }
+    }
+    
+    func toggleTorch(on: Bool) {
+        guard let device = AVCaptureDevice.default(for: .video) else { return }
+
+        if device.hasTorch {
+            do {
+                try device.lockForConfiguration()
+
+                if on == true {
+                    device.torchMode = .on
+                } else {
+                    device.torchMode = .off
+                }
+
+                device.unlockForConfiguration()
+            } catch {
+                print("Torch could not be used")
+            }
+        } else {
+            print("Torch is not available")
+        }
+    }
 }
 
 struct ReceiptLotteryView_Previews: PreviewProvider {
